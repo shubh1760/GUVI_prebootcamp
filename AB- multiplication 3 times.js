d2be554b7866df14273d3e0f54ac5cb9926cf470
Shubh <64539293+shubh1760@users.noreply.github.com>
2021-05-11 04:38:38 +0530

    Add files via upload

---
+++ AB- multiplication 3 times.js
@@ -0,0 +1,18 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  let a=userInput[0];
+  let b=a*a*a;
+  console.log(b);
+});
