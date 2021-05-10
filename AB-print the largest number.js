e42a06d6d685b7ca6049bd72a717ed5598830271
Shubh <64539293+shubh1760@users.noreply.github.com>
2021-05-11 05:09:06 +0530

    Add files via upload

---
+++ AB-print the largest number.js
@@ -0,0 +1,16 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+ console.log(Math.max(userInput[0],userInput[1],userInput[2]));
+});
